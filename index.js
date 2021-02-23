c70a466a4d558cfeba9ae10c0dafd0cbe3e37c24
Wesley <wspencer3295@yahoo.com>
2021-02-23 15:12:53 -0700

    commit tree 54497b828acfbdda4d2eb9d22649fc1940784898

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded",(e)=>{
+   
 
+})
+
+function superbowlWin(objectArry){
+let obj = objectArry.find(o => o.result === "W")
+if (!obj){
+return undefined
+}else{
+return obj.year
+}
+}
